ef67a17e8c246225b7c49e0fde6f85ed4480bbdb
arevalocarloseduardo <cae.arevalocarloseduardo@gmail.com>
2019-11-16 14:52:53 -0300

    validate email

--- lib/main.dart
+++ lib/main.dart
@@ -28,49 +28,58 @@ class _FormPageState extends State<FormPage> {
 
   String msg = "";
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Card(
         child: Padding(
           padding: const EdgeInsets.all(22.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                  flex: 2,
-                  child: Center(
-                    child: Text("Formulario de registro",
-                        style: TextStyle(fontSize: 28)),
-                  )),
-              TextFormField(
-                  validator: validate,
-                  controller: name,
-                  decoration: InputDecoration(
-                    labelText: "Nombre",
-                  )),
-              TextFormField(
-                  validator: validate,
-                  controller: email,
-                  decoration: InputDecoration(labelText: "E-mail")),
-              TextFormField(
-                  validator: validate,
-                  controller: pass,
-                  obscureText: true,
-                  decoration: InputDecoration(labelText: "Contraseña")),
-              SizedBox(
-                height: 20,
-              ),
-              FlatButton(
-                child: Text("Enviar", style: TextStyle(color: Colors.white)),
-                color: Colors.blue,
-                shape: StadiumBorder(),
-                onPressed: () => _addUser(),
-              ),
-              Expanded(
-                child: Text(msg),
-              ),
-            ],
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                    flex: 2,
+                    child: Center(
+                      child: Text("Formulario de registro",
+                          style: TextStyle(fontSize: 28)),
+                    )),
+                TextFormField(
+                    validator: validate,
+                    controller: name,
+                    decoration: InputDecoration(
+                      labelText: "Nombre",
+                    )),
+                TextFormField(
+                    validator: validate,
+                    controller: email,
+                    decoration: InputDecoration(labelText: "E-mail")),
+                TextFormField(
+                    validator: emailValidator,
+                    controller: pass,
+                    obscureText: true,
+                    decoration: InputDecoration(labelText: "Contraseña")),
+                SizedBox(
+                  height: 20,
+                ),
+                FlatButton(
+                  child: Text("Enviar", style: TextStyle(color: Colors.white)),
+                  color: Colors.blue,
+                  shape: StadiumBorder(),
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      _addUser();
+                    }
+                  },
+                ),
+                Expanded(
+                  child: Text(msg),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -78,9 +87,9 @@ class _FormPageState extends State<FormPage> {
   }
 
   void cleanData() {
-    name.dispose();
-    pass.dispose();
-    email.dispose();
+    name.clear();
+    pass.clear();
+    email.clear();
   }
 
   void _addUser() async {
@@ -92,14 +101,28 @@ class _FormPageState extends State<FormPage> {
     if (response.body == "OK") {
       setState(() {
         msg = "Usuario ${name.text} agregado con exito!";
-        cleanData();
       });
+      cleanData();
     }
   }
 
   String validate(String value) {
-    if (value?.isEmpty) {
-      return "el campo no puede estar vacio";
+    print(value);
+    if (value.isEmpty) {
+      return "El campo no puede estar vacio";
+    }
+    return null;
+  }
+
+  String emailValidator(String anEmailAddress) {
+    if (anEmailAddress.isEmpty) {
+      return "El campo no puede estar vacio";
+    }
+    if (RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(anEmailAddress)) {
+      return null;
+    } else {
+      return "Email Inválido";
     }
   }
 }
